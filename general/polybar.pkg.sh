5c02dc648256e6c8bed73374f2afbf2704915e73
Nicolas Beaussart <nic.beaussart@gmail.com>
2017-04-23 19:26:15 +0200

    switching to polybar and general update

---
+++ general/polybar.pkg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# https://github.com/jaagr/polybar/wiki/User-contributed-modules#archlinux-updates
+pac=$(checkupdates | wc -l)
+aur=$(cower -u | wc -l)
+
+check=$((pac + aur))
+if [[ "$check" != "0" ]]
+then
+    echo "$pac %{F#5b5b5b}%{F-} $aur"
+fi
