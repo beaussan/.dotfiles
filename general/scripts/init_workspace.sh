d6071fdf566e30a99231322051fd111cff416dd3
Nicolas Beaussart <nic.beaussart@gmail.com>
2023-09-23 21:18:48 +0200

    update configs

--- general/scripts/init_workspace.sh
+++ general/scripts/init_workspace.sh
@@ -26,54 +26,66 @@ workspace6="6:"
 #  random"
 workspace7="7:"
 
-# App you want to start :
-apps=(
- "telegram-desktop"
- "obsidian"
- "alacritty"
- "slack"
-)
-
-# Which workspace to assign your wanted App :
-workspaces=(
- $workspace1
- $workspace1
- $workspace2
- $workspace5
-)
-
-# counter of opened windows
-owNB=-1
-
-nativefier=$HOME/nativefier/bins
-
-# add paths of apps to array
-arr=()
-arr+=(
-    '/usr/bin/'
-    '/usr/bin/'
-    '/usr/bin/'
-    '/usr/bin/'
-)
-
-for iwin in ${!apps[*]}
-do
-    while [ "$owNB" -lt "$iwin" ] # wait before start other programs
-    do
-        owNB=$(wmctrl -l | wc -l) # Get number of actual opened windows
-        wait 5
+function wait_for_window {
+    local window_title="$1"
+    local window_id=""
+    
+    while [ -z "$window_id" ]; do
+        sleep 1
+        window_id=$(wmctrl -l | grep "$window_title" | cut -d ' ' -f 1)
     done
-    i3-msg workspace ${workspaces[$iwin]} # move in wanted workspace
-    ${arr[$iwin]}/${apps[$iwin]} & # start the wanted app
-done
+    
+    return 0
+}
 
+function wait_for_network {
+    local start_time=$(date +%s)
+    
+    # Wait for network to be online
+    while true; do
+        if ping -c 1 google.com &>/dev/null; then
+            return 0
+        fi
+        
+        # Check if timeout has been reached
+        local end_time=$(date +%s)
+        local elapsed=$((end_time - start_time))
+        if [ $elapsed -gt 30 ]; then
+            echo "Timed out waiting for network to be online"
+            return 1
+        fi
+        
+        sleep 1
+    done
+}
+
+
+# Workspace 1 content
 i3-msg workspace $workspace1
+i3-msg move workspace to output primary
 i3-msg move workspace to output left
+
+"$(ls /home/beaussan/bin/*beeper*)" & wait_for_window "Beeper"
+# /usr/bin/telegram-desktop & wait_for_window "Telegram"
+/usr/bin/obsidian & wait_for_window "Obsidian"
 i3-msg layout tabbed
 
-i3-msg workspace $workspace5
-i3-msg move workspace to output right
-i3-msg move workspace to output right
+if wait_for_network && sleep 1 && wait_for_network && sleep 1; then
 
-i3-msg workspace $workspace2
-i3-msg move workspace to output right
+    # Workspace 5 content
+    i3-msg workspace $workspace5
+    i3-msg move workspace to output primary
+    i3-msg move workspace to output right
+    /usr/bin/slack & wait_for_window "Slack"
+
+    # Workspace 2
+    i3-msg workspace $workspace2
+    i3-msg move workspace to output primary
+
+    # Network is online, continue script
+    /usr/bin/google-chrome-stable &
+else
+    # Network is offline, handle error
+    notify-send -u critical "Network is offline, cannot continue script."
+    exit 1
+fi
